747760010f8657cde79aa65b557ce70cea59d68e
Eduardo <eduardonaves02@outlook.com>
2022-07-26 16:10:38 -0300

    atualização do organo

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -5,7 +5,7 @@ const CampoTexto = (props) => {
     return (
         <div className="campo__texto"> 
             <label>{props.label}</label>
-            <input placeholder={props.placeholder}/>         
+            <input required={props.obrigatorio} placeholder={props.placeholder}/>         
         </div>
     )
 }
