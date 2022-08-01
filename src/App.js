cedfe5687868cf72e198a9d72dc75b7f98da111c
Eduardo <eduardonaves02@outlook.com>
2022-08-01 19:31:21 -0300

    Finazização do projeto em react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Rodape from './componentes/Rodape';
 import Time from './componentes/Time';
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
         corSecundaria:"#E8F8FF",
       },
       {
-        nome:" Data Science",
+        nome:"Data Science",
         corPrimaria:"#A6D157",
         corSecundaria:"#F0F8E2",
       },
@@ -58,11 +59,14 @@ function App() {
       <Banner />
       <Formulario  times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
       
-      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)} 
-        
-
-     
-      
+      {times.map(time => <Time 
+      key={time.nome} 
+      nome={time.nome} 
+      corPrimaria={time.corPrimaria} 
+      corSecundaria={time.corSecundaria}
+      colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)} 
+        <Rodape />
     </div>
   );
 } 
---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,18 @@
+import "./Rodape.css"
+
+const Rodape = () => {
+    return(
+        <div className="rodape">
+            <div className="redes__sociais">
+                <img src="./imagens/fb.png" alt="icone facebook png" />
+                <img src="./imagens/tw.png" alt="icone twitter png" />
+                <img src="./imagens/ig.png" alt="icone instagram png" />
+            </div>
+            <img src="./imagens/logo.png" alt="logo da organo" />
+            <p>Desenvolvido por Alura.</p>
+
+        </div>
+    )   
+}
+
+export default Rodape
